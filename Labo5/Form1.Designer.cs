37e070c96f78efc01be5e3412dae52a120fb4d25
Lourmati <49345524+Lourmati@users.noreply.github.com>
2020-10-12 23:43:12 -0400

    Initial addition of files

---
+++ Labo5/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Labo5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnNuage = new System.Windows.Forms.Button();
+            this.BtnPlancher = new System.Windows.Forms.Button();
+            this.BtnBrique = new System.Windows.Forms.Button();
+            this.BtnCoin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnNuage
+            // 
+            this.BtnNuage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnNuage.Location = new System.Drawing.Point(977, 101);
+            this.BtnNuage.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnNuage.Name = "BtnNuage";
+            this.BtnNuage.Size = new System.Drawing.Size(38, 41);
+            this.BtnNuage.TabIndex = 0;
+            this.BtnNuage.UseVisualStyleBackColor = true;
+            this.BtnNuage.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // BtnPlancher
+            // 
+            this.BtnPlancher.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnPlancher.Location = new System.Drawing.Point(977, 55);
+            this.BtnPlancher.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnPlancher.Name = "BtnPlancher";
+            this.BtnPlancher.Size = new System.Drawing.Size(38, 41);
+            this.BtnPlancher.TabIndex = 1;
+            this.BtnPlancher.UseVisualStyleBackColor = true;
+            this.BtnPlancher.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // BtnBrique
+            // 
+            this.BtnBrique.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnBrique.Location = new System.Drawing.Point(977, 10);
+            this.BtnBrique.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnBrique.Name = "BtnBrique";
+            this.BtnBrique.Size = new System.Drawing.Size(38, 41);
+            this.BtnBrique.TabIndex = 2;
+            this.BtnBrique.UseVisualStyleBackColor = true;
+            this.BtnBrique.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // BtnCoin
+            // 
+            this.BtnCoin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnCoin.Location = new System.Drawing.Point(977, 146);
+            this.BtnCoin.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnCoin.Name = "BtnCoin";
+            this.BtnCoin.Size = new System.Drawing.Size(38, 41);
+            this.BtnCoin.TabIndex = 3;
+            this.BtnCoin.UseVisualStyleBackColor = true;
+            this.BtnCoin.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 511);
+            this.Controls.Add(this.BtnCoin);
+            this.Controls.Add(this.BtnBrique);
+            this.Controls.Add(this.BtnPlancher);
+            this.Controls.Add(this.BtnNuage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Labo5";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnNuage;
+        private System.Windows.Forms.Button BtnPlancher;
+        private System.Windows.Forms.Button BtnBrique;
+        private System.Windows.Forms.Button BtnCoin;
+    }
+}
+
---
+++ Labo5/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestTilesetMario;
+
+/// <summary>
+/// Oussama Lourhmati
+/// </summary>
+namespace Labo5
+{
+    /// <summary>
+    /// Classe pour le Form du Mario Maker
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        Choix choix;
+        Bitmap[,] tab = new Bitmap[32, 16];
+        public Form1()
+        {
+            InitializeComponent();
+            Initialisation();
+        }
+
+        /// <summary>
+        /// Initialisation de mes boutons (choix) et du tableau de Bitmap qui contient les images du Mario Maker (ciel, plancher, etc.)
+        /// </summary>
+        private void Initialisation()
+        {
+            BtnNuage.BackgroundImage = TilesetImageGenerator.GetTile(4);
+            BtnBrique.BackgroundImage = TilesetImageGenerator.GetTile(1);
+            BtnPlancher.BackgroundImage = TilesetImageGenerator.GetTile(0);
+            BtnCoin.BackgroundImage = TilesetImageGenerator.GetTile(3);
+
+            for (int i = 0; i < tab.GetLength(0); i ++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j ++)
+                {
+                    if (j >= 14)
+                    {
+                        tab[i, j] = TilesetImageGenerator.GetTile(0);
+                    }
+                    else
+                    {
+                        tab[i, j] = TilesetImageGenerator.GetTile(2);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Paint du Form, dessine le tableau de bitmap contenant le ciel et le plancher
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            DoubleBuffered = true;
+            for (int i = 0; i < tab.GetLength(0); i ++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j ++)
+                {
+                    g.DrawImage(tab[i, j], i*32, j*32, 32, 32);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Évenement mouseClick, il y aura un changement sur le Form selon le choix de l'utilisateur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (choix)
+            {
+                case Choix.Brique:
+                    if (tab[e.X / 32, e.Y / 32] != TilesetImageGenerator.GetTile(1))
+                    {
+                        tab[e.X / 32, e.Y / 32] = TilesetImageGenerator.GetTile(1);
+                        Refresh();
+                    }
+                    break;
+                case Choix.Plancher:
+                    if (tab[e.X / 32, e.Y / 32] != TilesetImageGenerator.GetTile(0))
+                    {
+                        tab[e.X / 32, e.Y / 32] = TilesetImageGenerator.GetTile(0);
+                        Refresh();
+                    }
+                    break;
+                case Choix.Nuage:
+                    if (tab[e.X / 32, e.Y / 32] != TilesetImageGenerator.GetTile(4))
+                    {
+                        tab[e.X / 32, e.Y / 32] = TilesetImageGenerator.GetTile(4);
+                        Refresh();
+                    }
+                    break;
+                case Choix.Coin:
+                    if (tab[e.X / 32, e.Y / 32] != TilesetImageGenerator.GetTile(3))
+                    {
+                        tab[e.X / 32, e.Y / 32] = TilesetImageGenerator.GetTile(3);
+                        Refresh();
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Énum pour la sélection du choix, si il veut ajouter la brique, le plancher, le nuage ou le coin.
+        /// </summary>
+        private enum Choix
+        {
+            Brique,
+            Plancher,
+            Nuage,
+            Coin,
+        };
+
+
+        /// <summary>
+        /// Évenement clique si l'utilisateur choisi la brique
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Click(object sender, EventArgs e)
+        {
+
+            if (sender as Button == BtnBrique)
+            {
+               choix = Choix.Brique;
+            }
+
+            if (sender as Button == BtnPlancher)
+            {
+                choix = Choix.Plancher;
+            }
+
+            if (sender as Button == BtnNuage)
+            {
+                choix = Choix.Nuage;
+            }
+
+            if (sender as Button == BtnCoin)
+            {
+                choix = Choix.Coin;
+            }
+
+        }
+    }
+}
